e7c25df47631bf167a585ade84f1f24615109896
Stefano Ribaldone <sribaldone@gmail.com>
2026-05-17 18:46:04 +0200

    Aggiunte entità FavoriteVenue e VenueDrink

---
+++ localbrew/src/main/java/com/project/localbrew/entity/FavoriteVenue.java
@@ -0,0 +1,35 @@
+package com.project.localbrew.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Slf4j
+@Entity
+@Table(name = "favorite_venues", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "venue_id"}))
+public class FavoriteVenue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "saved_at", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime savedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "venue_id", nullable = false)
+    private Venue venue;
+}
