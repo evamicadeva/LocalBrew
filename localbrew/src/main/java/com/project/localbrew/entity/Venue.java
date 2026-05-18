1c6f29c0be7b4f9f10640d76bae2338bc5a2179f
Stefano Ribaldone <sribaldone@gmail.com>
2026-05-18 12:07:38 +0200

    Corretta entità Venue

--- localbrew/src/main/java/com/project/localbrew/entity/Venue.java
+++ localbrew/src/main/java/com/project/localbrew/entity/Venue.java
@@ -14,37 +14,37 @@ import java.util.UUID;
 @Entity
 @Table(name = "venues")
 public class Venue {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
 
-    @Column(nullable = false, length = 50)
-    private String name;
+	@Column(nullable = false, length = 50)
+	private String name;
 
-    @Column(length = 500)
-    private String desription;
+	@Column(length = 500)
+	private String desription;
 
-    @Column(nullable = false, length = 70)
-    private String adress;
+	@Column(nullable = false, length = 70)
+	private String adress;
 
-    @Column(nullable = false)
-    private Double latitude;
+	@Column(nullable = false)
+	private Double latitude;
 
-    @Column(nullable = false)
-    private Double longitude;
+	@Column(nullable = false)
+	private Double longitude;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private VenueType type;
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private VenueType type;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private StatusVenue status;
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private StatusVenue status;
 
-    @Column(nullable = false)
-    private LocalDate createdAt;
+	@Column(nullable = false)
+	private LocalDate createdAt;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User owner;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User owner;
 }
