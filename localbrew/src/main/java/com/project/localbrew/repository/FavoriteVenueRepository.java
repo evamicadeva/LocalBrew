74780068d021b521ec2ef1baedc4d9caf5e6a5d8
evamicadeva <229135266+evamicadeva@users.noreply.github.com>
2026-05-18 17:08:39 +0200

    Extend FavoriteVenueRepository to use JpaRepository

--- localbrew/src/main/java/com/project/localbrew/repository/FavoriteVenueRepository.java
+++ localbrew/src/main/java/com/project/localbrew/repository/FavoriteVenueRepository.java
@@ -1,5 +1,13 @@
 package com.project.localbrew.repository;
 
-public interface FavoriteVenueRepository {
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.localbrew.entity.FavoriteVenue;
+
+@Repository
+public interface FavoriteVenueRepository extends JpaRepository<FavoriteVenue, UUID> {
 
 }
