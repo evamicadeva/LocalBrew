26033bc9d9db31a9d9dbb452ab003bed3ad6d7db
evamicadeva <229135266+evamicadeva@users.noreply.github.com>
2026-05-18 17:11:02 +0200

    Extend VenueRepository to use JpaRepository

--- localbrew/src/main/java/com/project/localbrew/repository/VenueRepository.java
+++ localbrew/src/main/java/com/project/localbrew/repository/VenueRepository.java
@@ -1,5 +1,12 @@
 package com.project.localbrew.repository;
+import java.util.UUID;
 
-public interface VenueRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.localbrew.entity.Venue;
+
+@Repository
+public interface VenueRepository extends JpaRepository<Venue, UUID> {
 
 }
