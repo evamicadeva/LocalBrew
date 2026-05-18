64c2861507441988d1e7af9da002e11c66f99fd3
Gabriel Roncolato <gabrielroncolato0007@gmail.com>
2026-05-18 16:24:47 +0200

    Creato metodo saveVenue e findUserInsade su VenueServiceImpl

--- localbrew/src/main/java/com/project/localbrew/service/VenueServiceImpl.java
+++ localbrew/src/main/java/com/project/localbrew/service/VenueServiceImpl.java
@@ -4,14 +4,21 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+
+import com.project.localbrew.entity.User;
 import com.project.localbrew.entity.Venue;
+import com.project.localbrew.repository.UserRepository;
 import com.project.localbrew.repository.VenueRepository;
+
+import jakarta.transaction.Transactional;
 
 public class VenueServiceImpl implements VenueService {
 	private final VenueRepository venueRepository;
+	private final UserRepository userRepository;
 
-	public VenueServiceImpl(VenueRepository venueRepository) {
+	public VenueServiceImpl(VenueRepository venueRepository, UserRepository userRepository) {
 		this.venueRepository = venueRepository;
+		this.userRepository = userRepository;
 	}
 
 	@Override
@@ -21,17 +28,26 @@ public class VenueServiceImpl implements VenueService {
 
 	@Override
 	public Venue findVenueById(UUID id) {
-		if(id == null) {
+		if (id == null) {
 			// Mettere eccezione personalizzata per id non trovato
 		}
 		Optional<Venue> optVenue = venueRepository.findById(id);
-		return optVenue.orElseThrow(() -> new Exception());// mettere eccezione personalizzata per Venue non trovato con quel id
+		return optVenue.orElseThrow(() -> new Exception());// mettere eccezione personalizzata per Venue non trovato con
+															// quel id
 	}
 
+	@Transactional
 	@Override
 	public Venue saveVenue(Venue venue) {
-		// TODO Auto-generated method stub
-		return null;
+		if (venue == null) {
+			// eccezione personalizzata per venue = null
+		}
+		
+		// Cercare lo user
+		User user = findUserInsideVenue(venue);
+		venue.setOwner(user);
+		
+		return venueRepository.save(venue);
 	}
 
 	@Override
@@ -46,4 +62,11 @@ public class VenueServiceImpl implements VenueService {
 
 	}
 
+	// metodi privati
+	// validazione
+	// serve per controllare se lo user esiste
+	private User findUserInsideVenue(Venue venue) { 
+		return userRepository.findById(venue.getOwner().getId())
+				.orElseThrow(() -> new IllegalArgumentException("User non trovato con id:" + venue.getOwner().getId()));
+	}// cambiare illegal exceptio con una personalizzata
 }
