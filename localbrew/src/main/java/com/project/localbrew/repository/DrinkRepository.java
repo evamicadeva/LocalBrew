88cf071a7d673e7a72bda0b1570b664cb66acae2
evamicadeva <229135266+evamicadeva@users.noreply.github.com>
2026-05-18 17:06:29 +0200

    Extend DrinkRepository to use JpaRepository

--- localbrew/src/main/java/com/project/localbrew/repository/DrinkRepository.java
+++ localbrew/src/main/java/com/project/localbrew/repository/DrinkRepository.java
@@ -1,5 +1,12 @@
 package com.project.localbrew.repository;
+import java.util.UUID;
 
-public interface DrinkRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.localbrew.entity.Drink;
+
+@Repository
+public interface DrinkRepository extends JpaRepository<Drink, UUID> {
 
 }
