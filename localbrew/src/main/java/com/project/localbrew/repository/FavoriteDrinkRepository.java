be1a037cb658012a7e42c23400d490c83b7958ff
evamicadeva <229135266+evamicadeva@users.noreply.github.com>
2026-05-18 17:07:45 +0200

    Extend FavoriteDrinkRepository to use JpaRepository

--- localbrew/src/main/java/com/project/localbrew/repository/FavoriteDrinkRepository.java
+++ localbrew/src/main/java/com/project/localbrew/repository/FavoriteDrinkRepository.java
@@ -1,5 +1,13 @@
 package com.project.localbrew.repository;
 
-public interface FavoriteDrinkRepository {
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.localbrew.entity.FavoriteDrink;
+
+
+public interface FavoriteDrinkRepository extends JpaRepository<FavoriteDrink, UUID> {
 
 }
