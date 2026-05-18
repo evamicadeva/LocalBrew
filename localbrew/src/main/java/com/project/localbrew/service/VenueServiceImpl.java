6ce9b7ca3f5ce53119e4bff96cc2a42422948dce
Gabriel Roncolato <gabrielroncolato0007@gmail.com>
2026-05-18 17:08:54 +0200

    Creazione del mettodo updateById su VenueServiceImpl

--- localbrew/src/main/java/com/project/localbrew/service/VenueServiceImpl.java
+++ localbrew/src/main/java/com/project/localbrew/service/VenueServiceImpl.java
@@ -32,8 +32,9 @@ public class VenueServiceImpl implements VenueService {
 			// Mettere eccezione personalizzata per id non trovato
 		}
 		Optional<Venue> optVenue = venueRepository.findById(id);
-		return optVenue.orElseThrow(() -> new Exception());// mettere eccezione personalizzata per Venue non trovato con
-															// quel id
+		
+		// mettere eccezione personalizzata per Venue non trovato con quel id
+		return optVenue.orElseThrow(() -> new Exception());
 	}
 
 	@Transactional
@@ -50,12 +51,44 @@ public class VenueServiceImpl implements VenueService {
 		return venueRepository.save(venue);
 	}
 
+	@Transactional
 	@Override
-	public Venue updateVenueById(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Venue updateVenueById(Venue venue , UUID id) {
+		if (id == null) {
+			// Mettere eccezione personalizzata per id non trovato
+		}
+		if (venue == null) {
+			// eccezione personalizzata per venue = null
+		}
+		
+		//verifica se esiste la Venue
+		//cambiare exception con un eccezione personalizata
+		Venue nwVenue = venueRepository.findById(id).orElsethrow(() -> new IllegalArgumentException());
+		
+		//controlli
+		if(venue.getName() != null && !venue.getName().isBlank())
+			nwVenue.setName(venue.getName());
+		if(venue.getAdress() != null && !venue.getAdress().isBlank())
+			nwVenue.setAdress(venue.getAdress());
+		if(venue.getLatitude() != null)
+			nwVenue.setLatitude(venue.getLatitude());
+		if(venue.getLongitude() != null)
+			nwVenue.setLongitude(venue.getLongitude());
+		if(venue.getType() != null)
+			nwVenue.setType(venue.getType());
+		if(venue.getStatus() != null)
+			nwVenue.setStatus(venue.getStatus());
+		if(venue.getCreatedAt() != null)
+			nwVenue.setCreatedAt(venue.getCreatedAt());
+		if(venue.getOwner() != null) {
+			User user = findUserInsideVenue(venue);
+			nwVenue.setOwner(user);
+		}
+		
+		return nwVenue;
 	}
 
+	@Transactional
 	@Override
 	public void deleteVenue(UUID id) {
 		// TODO Auto-generated method stub
@@ -68,5 +101,5 @@ public class VenueServiceImpl implements VenueService {
 	private User findUserInsideVenue(Venue venue) { 
 		return userRepository.findById(venue.getOwner().getId())
 				.orElseThrow(() -> new IllegalArgumentException("User non trovato con id:" + venue.getOwner().getId()));
-	}// cambiare illegal exceptio con una personalizzata
+	}// cambiare illegal exception con una personalizzata
 }
