a9529aeebc66419db7da2c4f180e308196e7e7e9
Gabriel Roncolato <gabrielroncolato0007@gmail.com>
2026-05-18 15:15:08 +0200

    Modifica findbyUUid su Venue Service

--- localbrew/src/main/java/com/project/localbrew/service/VenueService.java
+++ localbrew/src/main/java/com/project/localbrew/service/VenueService.java
@@ -1,11 +1,12 @@
 package com.project.localbrew.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.project.localbrew.entity.Venue;
 
 public interface VenueService {
 	List<Venue> findAllVenues();
-	Venue findVenueByUUID();
+	Venue findVenueById(UUID id);
 	Venue saveVenue(Venue venue);
 }
