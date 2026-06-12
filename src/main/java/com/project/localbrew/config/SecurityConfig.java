7e041df0372a63a180bdc8b3dee886bb7964aed0
Stefano Ribaldone <sribaldone@gmail.com>
2026-06-12 19:08:08 +0200

    Fixes

--- src/main/java/com/project/localbrew/config/SecurityConfig.java
+++ src/main/java/com/project/localbrew/config/SecurityConfig.java
@@ -41,8 +41,7 @@ public class SecurityConfig {
                 "http://localhost:8080",
                 "http://127.0.0.1:8080",
                 "http://localhost:5500",
-                "http://127.0.0.1:5500",
-                "https://demanding-trembl-regarded-myers.trycloudflare.com/"
+                "http://127.0.0.1:5500"
         ));
         configuration.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
